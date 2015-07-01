d9c9dd7e0a49a0b7d2e3126bcd9f4046cc837ef6
PriscillaP <Priscillapeter@hotmail.com>
2015-06-30 20:10:33 -0500

    lskjdflj

--- script.js
+++
@@ -1,6 +0,0 @@
- document.getElementById("mylink").style.fontWeight = "bold"; 
-console.log("I'm logging things!");
-
-document.getElementById("myspan").onclick= Function() {
-    alert('this is a test');
-};
